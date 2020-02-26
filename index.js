7ee41d0d5109eb9432eb7a50a54c1a16dadf6967
lauripalonen <palonen.lauri@gmail.com>
2020-02-26 10:39:30 +0200

    init backend

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const db = require('./db.json')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+const packages = db
+
+app.get('/', (req, res) => {
+  res.send('<h1>dpkg package library</h1>')
+})
+
+app.get('/api/packages', (req, res) => {
+  res.json(packages)
+})
+
+app.get('/api/packages/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const package = packages.find(pkg => {
+    console.log(pkg)
+    return (
+      pkg.id === id
+    )
+  })
+
+  if (package) {
+    response.json(package)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
