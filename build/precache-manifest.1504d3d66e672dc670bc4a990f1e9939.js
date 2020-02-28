e171d42fe009b52eed08e9a394680ffe523997b9
lauripalonen <palonen.lauri@gmail.com>
2020-02-28 19:27:43 +0200

    create build for heroku

---
+++ build/precache-manifest.1504d3d66e672dc670bc4a990f1e9939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bd23d5418c8841b99e9d8ee234c8b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34703db71a8f302ff46",
+    "url": "/static/css/main.6de89c4e.chunk.css"
+  },
+  {
+    "revision": "07fd4841658ddac8dd55",
+    "url": "/static/js/2.cecea094.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.cecea094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34703db71a8f302ff46",
+    "url": "/static/js/main.eb795b6c.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
